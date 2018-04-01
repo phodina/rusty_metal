e557506713563d5869b6dc83c3b563062d59d0c8
Petr Hodina <hodinapetr46@gmail.com>
2018-04-01 11:29:12 +0200

    Implement frame allocator based on multiboot.

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,13 @@ extern crate volatile;
 extern crate spin;
 extern crate multiboot2;
 
-use multiboot2::BootInformation;
-use multiboot2::ElfSectionsTag;
+use multiboot2::{BootInformation, MemoryMapTag, ElfSectionsTag};
 
 #[macro_use]
 mod vga;
+mod memory;
+use memory::area_frame_allocator::AreaFrameAllocator;
+use memory::FrameAllocator;
 
 #[no_mangle]
 pub extern "C" fn rust_main(multiboot_information_address: usize) {
@@ -18,7 +20,6 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
 
     let boot_info = get_boot_info(multiboot_information_address);
     let elf_sections_tag = get_elf_sections(&boot_info);
-
     
     let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
         .min().unwrap();
@@ -28,9 +29,22 @@ pub extern "C" fn rust_main(multiboot_information_address: usize) {
     let multiboot_start = multiboot_information_address;
     let multiboot_end = multiboot_start + (boot_info.total_size as usize);
 
+    let memory_map_tag = get_memory_map_tag(&boot_info);
+    
+    let mut frame_allocator = AreaFrameAllocator::new(kernel_start as usize, kernel_end as usize, multiboot_start, multiboot_end, memory_map_tag.memory_areas());
+
+    for i in 0.. {
+        if let None = frame_allocator.allocate_frame() {
+            println!("allocated {} frames", i);
+            break;
+        }
+    }
+
+    //println!("Frame: {:?}", frame_allocator.allocate_frame());
+    
     println!("Kernel start: 0x{:x} end: 0x{:x}", kernel_start, kernel_end);
     println!("Multiboot start: 0x{:x} end: 0x{:x}", multiboot_start, multiboot_end);
-    
+
     loop{}
 }
 
@@ -38,10 +52,13 @@ fn get_boot_info(multiboot_information_address: usize) -> &'static BootInformati
     unsafe{ multiboot2::load(multiboot_information_address) }
 }
 
-fn print_memory_sections(boot_info: &'static BootInformation) {
+fn get_memory_map_tag(boot_info: &'static BootInformation) -> &'static MemoryMapTag {
 
-    let memory_map_tag = boot_info.memory_map_tag()
-        .expect("Memory map tag required");
+    boot_info.memory_map_tag()
+        .expect("Memory map tag required")
+}
+
+fn print_memory_sections(memory_map_tag: &MemoryMapTag) {
 
     println!("memory areas:");
     for area in memory_map_tag.memory_areas() {
