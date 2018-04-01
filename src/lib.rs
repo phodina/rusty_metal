68e0f0c64a85c8ceac7d8d41dcc3569cec87dd62
Petr Hodina <hodinapetr46@gmail.com>
2018-04-01 10:52:39 +0200

    Calculate siz of multiboot & kernel.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ extern crate volatile;
 extern crate spin;
 extern crate multiboot2;
 
-use core::fmt::Write;
+use multiboot2::BootInformation;
+use multiboot2::ElfSectionsTag;
 
 #[macro_use]
 mod vga;
@@ -14,15 +15,31 @@ mod vga;
 pub extern "C" fn rust_main(multiboot_information_address: usize) {
 
     vga::clear_screen();
+
+    let boot_info = get_boot_info(multiboot_information_address);
+    let elf_sections_tag = get_elf_sections(&boot_info);
+
     
-    print_sections(multiboot_information_address);
+    let kernel_start = elf_sections_tag.sections().map(|s| s.addr)
+        .min().unwrap();
+    let kernel_end = elf_sections_tag.sections().map(|s| s.addr + s.size)
+        .max().unwrap();
+
+    let multiboot_start = multiboot_information_address;
+    let multiboot_end = multiboot_start + (boot_info.total_size as usize);
+
+    println!("Kernel start: 0x{:x} end: 0x{:x}", kernel_start, kernel_end);
+    println!("Multiboot start: 0x{:x} end: 0x{:x}", multiboot_start, multiboot_end);
     
     loop{}
 }
 
-fn print_sections(multiboot_information_address: usize) {
+fn get_boot_info(multiboot_information_address: usize) -> &'static BootInformation {
+    unsafe{ multiboot2::load(multiboot_information_address) }
+}
 
-    let boot_info = unsafe{ multiboot2::load(multiboot_information_address) };
+fn print_memory_sections(boot_info: &'static BootInformation) {
+
     let memory_map_tag = boot_info.memory_map_tag()
         .expect("Memory map tag required");
 
@@ -31,9 +48,15 @@ fn print_sections(multiboot_information_address: usize) {
         println!("    start: 0x{:x}, length: 0x{:x}",
                  area.base_addr, area.length);
     }
+}
 
-    let elf_sections_tag = boot_info.elf_sections_tag()
-        .expect("Elf-sections tag required");    
+fn get_elf_sections(boot_info: &'static BootInformation) -> &'static ElfSectionsTag {
+    boot_info.elf_sections_tag()
+        .expect("Elf-sections tag required")    
+}
+
+fn print_elf_sections(elf_sections_tag: &'static ElfSectionsTag) {
+    
     println!("kernel sections:");
 
     for section in elf_sections_tag.sections() {
